09bf1899de275f3049849f7813f168a6b3a337b3
Furkan Acar <f.acar@azerion.com>
2022-11-05 20:07:52 +0300

    Updated modal: with dynamic content and functionality

--- modal/main.js
+++ modal/main.js
@@ -1,19 +1,43 @@
-const modalBtn = document.querySelector(".modalBtn");
-const modalOverlay = document.querySelector(".modal-overlay");
-const closeIcon = modalOverlay.querySelector(".close-icon");
-const acceptBtn = modalOverlay.querySelector(".acceptBtn");
-const cancelBtn = modalOverlay.querySelector(".cancelBtn");
+const custom_modal = (_title, _content, accepted, cancelled) => {
+  const modalBtn = document.querySelector(".modalBtn");
+  const modalOverlay = document.querySelector(".modal-overlay");
+  const title = modalOverlay.querySelector(".title");
+  const content = modalOverlay.querySelector(".content");
+  const closeIcon = modalOverlay.querySelector(".close-icon");
+  const acceptBtn = modalOverlay.querySelector(".acceptBtn");
+  const cancelBtn = modalOverlay.querySelector(".cancelBtn");
 
-modalBtn.addEventListener("click", () => (modalOverlay.style.display = "flex"));
-closeIcon.addEventListener(
-  "click",
-  () => (modalOverlay.style.display = "none")
-);
-acceptBtn.addEventListener("click", () => {
+  title.innerHTML = _title;
+  _content = document.querySelector(_content);
+  content.appendChild(_content);
+
+  modalBtn.addEventListener(
+    "click",
+    () => (modalOverlay.style.display = "flex")
+  );
+
+  const closeModal = () => {
+    modalOverlay.style.display = "none";
+  };
+
+  closeIcon.addEventListener("click", () => closeModal());
+
+  acceptBtn.addEventListener("click", () => {
+    accepted();
+    closeModal();
+  });
+
+  cancelBtn.addEventListener("click", () => {
+    cancelled();
+    closeModal();
+  });
+};
+
+const triggeredAccept = () => {
   console.log("Accepted");
-  modalOverlay.style.display = "none";
-});
-cancelBtn.addEventListener("click", () => {
+};
+const triggeredCancelled = () => {
   console.log("Cancelled");
-  modalOverlay.style.display = "none";
-});
+};
+
+custom_modal("Modal Sample", "#content", triggeredAccept, triggeredCancelled);
